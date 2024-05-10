d31b1f5a6bb0ec88ff605338b7ab4eb49f49d7f5
Saad Khurshid Qurashi <147279806+Isaadqurashi@users.noreply.github.com>
2024-05-11 00:36:33 +0500

    Update Tic Tac Toe With GUI.cpp

--- Tic Tac Toe With GUI.cpp
+++ Tic Tac Toe With GUI.cpp
@@ -1,1 +1,145 @@
 //
+// Tic Tac Toe With GUI.cpp : Defines the entry point for the application.
+//
+#include "framework.h"
+#include "Tic Tac Toe With GUI.h"
+#define MAX_LOADSTRING 100
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    // TODO: Place code here.
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_TICTACTOEWITHGUI, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TICTACTOEWITHGUI));
+    MSG msg;
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+    return (int) msg.wParam;
+}
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TICTACTOEWITHGUI));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+   //wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.hbrBackground = (HBRUSH)(GetStockObject(GRAY_BRUSH));
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TICTACTOEWITHGUI);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+@@ -121,7 +122,25 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+//  WM_DESTROY  - post a quit message and return
+//
+//
+const int CELL_SIZE = 100; //gloabl variable for our game
+const int CELL_SIZE = 100; //global variable for our game
+bool GetGameBoardRect(HWND hwnd, RECT* pRect)
+{
+    RECT rc; // rectangle structure
+    if (GetClientRect(hwnd, &rc))
+    {
+        int width = rc.right - rc.left;
+        int height = rc.bottom - rc.top;
+
+        pRect->left = (width - CELL_SIZE * 3) / 2;
+        pRect->top = (height - CELL_SIZE * 3) / 2;
+
+        pRect->right = pRect->left + CELL_SIZE * 3;
+        pRect->bottom = pRect->top + CELL_SIZE * 3;
+        return TRUE;
+    }
+    SetRectEmpty(pRect);
+    return FALSE;
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+@@ -157,18 +176,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            RECT rc; // rectangle structure
+            if (GetClientRect(hWnd, &rc))
+            {
+                int width = rc.right - rc.left;
+                int height = rc.bottom - rc.top;
+
+                int left = (width - CELL_SIZE * 3) / 2;
+                int top = (height - CELL_SIZE * 3) / 2;
+            bool GetGameBoardRect(HWND hwnd, RECT * pRect);
+
+                int right = left + CELL_SIZE * 3;
+                int bottom = top + CELL_SIZE * 3;
+            if (GetClientRect(hWnd, &rc))
+            {
+
+                Rectangle(hdc, left, top, right, bottom);
+                Rectangle(hdc, rc.left, rc.top, rc.right, rc.bottom);
+            }
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
